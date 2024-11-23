f9548c3b3a6b8b65d20e7f43fd10d13cc78f3eeb
Alexandre Burghesi <alexandreburghesi@hotmail.com>
2024-11-23 12:02:30 -0300

    Mudando porta do front para 9000

--- frontend/app/vue.config.js
+++ frontend/app/vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
+    port: 9000, // CHANGE YOUR PORT HERE!
     proxy: {
       '/api': {
         target: process.env.VUE_APP_BACKEND_URL,  // Usa a URL dinâmica do backend
